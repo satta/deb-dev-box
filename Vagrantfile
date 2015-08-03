e1cbc7907ab7b8348d02085537a58a300a069295
Sascha Steinbiss <sascha@steinbiss.name>
2015-08-03 20:05:24 +0100

    finish up first version

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "binarydata/debian-jessie"
+  config.vm.box = "debian/jessie64"
   config.vm.box_check_update = false
 
   # Create a public network, which generally matched to bridged network.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
+# quick'n'dirty provisioning -- this isn't particularly elegant
+# but will do for now
+
+# use unstable too please
 export DEBIAN_FRONTEND=noninteractive
 echo "deb http://http.us.debian.org/debian unstable main non-free contrib" >> /etc/apt/sources.list
 echo "Package: *"  > /etc/apt/preferences.d/pin_unstable
 echo "Pin: release a=unstable" >> /etc/apt/preferences.d/pin_unstable
 echo "Pin-Priority: -99" >> /etc/apt/preferences.d/pin_unstable
 apt-get update
-apt-get install -y pbuilder gdb lua5.1 stow
-apt-get install -y libconfig-model-dpkg-perl libconfig-model-itself-perl devscripts git-buildpackage svn-buildpackage
-apt-get install -y help2man git-buildpackage svn-buildpackage subversion git
+
+# install base dev packages
+apt-get install -y pbuilder gdb lua5.1 stow libconfig-model-dpkg-perl \
+                   libconfig-model-itself-perl devscripts git-buildpackage \
+                   svn-buildpackage help2man subversion git build-essential \
+                   gnupg reportbug libgenometools0-dev libcairo2-dev
 apt-get install -y -t unstable cme lintian
+
+# dotfiles
 cd /home/vagrant
 git clone https://github.com/satta/dotfiles.git
 cd /home/vagrant/dotfiles
@@ -21,8 +30,15 @@ stow git
 stow debian
 rm -rf /home/vagrant/.ccache
 stow ccache
+
+# GPG keys
 ln -fs /homedir/.gnupg /home/vagrant
+
+# SSH keys
 cp /homedir/.ssh/id_rsa* /home/vagrant/.ssh
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
+
+# sid base tarball for pbuilder
 mkdir -p /var/cache/pbuilder
 cp /homedir/uni/debian-work/pbuilder-base/sid-amd64-base.tgz /var/cache/pbuilder
 ln -sf /home/vagrant/.pbuilder* /root
