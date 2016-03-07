4d3f1c487d4f13bcaffd80ec23f8aa6860ae5c53
Sascha Steinbiss <sascha@steinbiss.name>
2016-03-07 22:15:56 +0000

    update packaging VM

--- Vagrantfile
+++ Vagrantfile
@@ -3,23 +3,23 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  # config.vm.box = "ffuenf/debian-8.0.0-amd64"
-  config.vm.box = "debian-jessie"
-  config.vm.box_check_update = false
-  config.vbguest.auto_update = false
-
-  # config.vm.network "public_network", bridge: "wlan0"
-  config.vm.network "private_network", type: "dhcp"
-  config.vm.network :forwarded_port, guest: 8000, host: 8800
+  config.vm.box = "debian-testing"
+ # config.vm.box_check_update = false
+ # config.vbguest.auto_update = false
+  config.vm.network :private_network, ip: "10.11.12.13"
 
   config.ssh.forward_agent = true
   config.ssh.forward_x11 = true
 
   config.vm.synced_folder "/Users/satta", "/homedir"
+  config.vm.synced_folder ".", "/vagrant", type: :nfs
+  config.bindfs.bind_folder "/vagrant", "/vagrant"
 
   config.vm.provider "virtualbox" do |vb|
-    vb.memory = 2048
+    vb.memory = 3072
     vb.cpus = 2
+#    vb.customize ['modifyvm', :id, '--usb', 'on']
+#    vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'Yubikey Neo', '--vendorid', '0x1050', '--productid', '0x0111']
   end
 
   config.vm.provision :shell, path: "bootstrap.sh"
--- bootstrap.sh
+++ bootstrap.sh
@@ -12,9 +12,9 @@ apt-get install -y pbuilder gdb lua5.1 stow libconfig-model-dpkg-perl \
                    libconfig-model-itself-perl devscripts git-buildpackage \
                    svn-buildpackage help2man subversion git build-essential \
                    gnupg reportbug libgenometools0-dev libcairo2-dev vim zsh \
-                   cowbuilder cowdancer ccache valgrind texlive-full aptitude \
+                   cowbuilder cowdancer ccache valgrind aptitude \
                    quilt htop autopkgtest debci python-stdeb python3-stdeb \
-                   gitk git-gui
+                   gitk git-gui mbr vmdebootstrap signing-party
 apt-get install -y -t unstable cme lintian
 
 # dotfiles
@@ -29,34 +29,48 @@ stow zsh
 stow git
 stow debian
 stow ssh
+stow caff
 rm -rf /home/vagrant/.ccache
 stow ccache
 
 # use zsh
 chsh -s /usr/bin/zsh vagrant
 
-# GPG keys
-ln -fs /homedir/.gnupg /home/vagrant
+# GPG directory
+cp -prv /homedir/.gnupg /home/vagrant
+sed -i 's/^keyserver-options ca-cert-file.*/keyserver-options ca-cert-file=\/home\/vagrant\/.gnupg\/sks-keyservers.netCA.pem/g' /home/vagrant/.gnupg/gpg.conf
 
 # SSH keys
 ln -fs /homedir/.ssh /home/vagrant/.ssh/satta
 
+# make local package directory
+mkdir /home/vagrant/_pbuilder_local_deps
+
+# give vagrant subdirectory to user
+chown -R vagrant:vagrant /home/vagrant
+
 # create base image for git-pbuilder/cowbuilder (nice to use with gbp)
-git-pbuilder create
+#mkdir /var/cache/pbuilder/base.cow
+#DIST=sid pbuilder create --configfile /home/vagrant/.pbuilderrc \
+#	  --buildplace /var/cache/pbuilder/base.cow \
+#		--mirror http://httpredir.debian.org/debian/ \
+#		--architecture amd64 --distribution sid --no-targz \
+#		--extrapackages cowdancer
 
 # create base image for debci/adt
-debci setup
-cat <<EOF > /etc/schroot/chroot.d/debci-unstable-amd64
-[debci-unstable-amd64]
-type=directory
-profile=debci
-description=debci unstable/amd64 chroot
-directory=/usr/share/debci/chroots/debci-unstable-amd64
-users=debci,vagrant
-groups=debci
-root-users=debci,vagrant
-source-root-users=debci,vagrant
-root-groups=root
-union-type=aufs
-union-overlay-directory=/dev/shm
-EOF
+#debci setup
+#cat <<EOF > /etc/schroot/chroot.d/debci-unstable-amd64
+#[debci-unstable-amd64]
+#type=directory
+#profile=debci
+#description=debci unstable/amd64 chroot
+#directory=/usr/share/debci/chroots/debci-unstable-amd64
+#users=debci,vagrant
+#groups=debci
+#root-users=debci,vagrant
+#source-root-users=debci,vagrant
+#root-groups=root
+#union-type=aufs
+#union-overlay-directory=/dev/shm
+#EOF
+
